941398ce8b5afbd5b9b116bc84a6e973fea4d04c
vardhamaan19 <80203030+vardhamaan19@users.noreply.github.com>
2021-07-19 19:38:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+
+
+function preload()
+{
+    clown_nose=loadImage("https://i.postimg.cc/cJBQFBPJ/ew.png");
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("posenet is initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {console.log(results);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y+5;
+    }
+    console.log("nose X="+results[0].pose.nose.x);
+    console.log("nose-y"+results[0].pose.nose.y);
+    }
+
+function draw()
+{
+image(video,0,0,300,300);
+image(clown_nose,noseX,noseY,30,30);
+}
+
+
+
+function take_snapshot()
+{
+    save('image.png');
+}
